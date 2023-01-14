8cdd20cd0e5a7567ea49b57fe8c9b71f6d9efa1b
Ali Keshvari <alikeshvari1400@gmail.com>
2023-01-14 23:06:43 +0330

    finish

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -6,19 +6,51 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly List<Service> _services = new List<Service>
+        {
+            new Service(1,"نقره ای"),
+            new Service(2,"طلایی"),
+            new Service(3,"پلاتین"),
+            new Service(4,"الماس")
+        };
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]
         public IActionResult Contact()
         {
-            return View();
+            var model = new Contact()
+            {
+                Services = new SelectList(_services,"Id","Name")
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+            model.Services = new SelectList(_services, "Id", "Name");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.error = "اطلاعات وارد شده صحیح نمی باشد لطفا دو مرتبه تلاش نمایید";
+                return View(model);
+            }
+            ModelState.Clear();
+            model = new Contact()
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+
+            ViewBag.success = "پیغام شما با موفقیت ارسال شد . باتشکر";
+            return View(model);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DevCard_MVC/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [MinLength(3,ErrorMessage = "حداقل طول باید 3 باشد")]
+        [MaxLength(100,ErrorMessage = "حداکثر طول باید 100 باشد")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [EmailAddress(ErrorMessage = "مقدار ایمیل وارد شده صحیح نمی باشد")]
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public int Service { get; set; }
+        public SelectList Services { get; set; }
+    }
+}
